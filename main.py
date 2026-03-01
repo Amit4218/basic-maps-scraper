58c0cf6a7f69d61fcb462d5bd2f59fe252a7e959
Amit4218 <amitbhagat621@gmail.com>
2026-03-01 19:53:43 +0530

    added detailed description

--- main.py
+++ main.py
@@ -4,23 +4,42 @@ import typer
 
 from src.scraper import scrape_only_business, scrape_using_keyword
 
-app = typer.Typer()
+app = typer.Typer(help="Basic Google Scraper CLI\n")
 
 
-@app.command()
+@app.command(help="Scrape businesses by keyword and location.")
 def keyword(
-    place: Annotated[str, typer.Argument(help="name of the location or place")],
-    keyword: Annotated[
-        str, typer.Argument(help="keyword to search (example Resturants)")
-    ],
+    place: Annotated[str, typer.Argument(help="Name of the location or place")],
+    keyword: Annotated[str, typer.Argument(help="Keyword to search eg: (Restaurants)")],
 ):
+    """
+    Search Google using a keyword within a specific place.\n
+    Example:
+    >>> main.py keyword <placename> <keyword>
+    """
     scrape_using_keyword(keyword, place)
 
 
-@app.command()
+@app.command(help="Scrape a specific business by name.")
 def business(
-    business_name: Annotated[List[str], typer.Argument(help="Name of the business")],
+    business_name: Annotated[
+        List[str],
+        typer.Argument(help="Full name of the business eg: (my business)"),
+    ],
+    scrape_reviews: bool = typer.Option(
+        False,
+        "--reviews",
+        help="scrape all the reviews (not implemented)",
+    ),
+    scrape_images: bool = typer.Option(
+        False, "--images", help="scrapes all the listed images link (not implemented)"
+    ),
 ):
+    """
+    Search and scrape a single business.
+    Example:
+    >>> main.py keyword <businessname>
+    """
     business = " ".join(business_name)
     scrape_only_business(business, headless=False)
 
